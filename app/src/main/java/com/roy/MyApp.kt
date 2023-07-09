3052525fdb4e5868c705ce4ea8a76cc3979f51b8
ChauTriTin <freuss47@gmail.com>
2023-07-09 12:35:20 +0700

    update

--- app/src/main/java/com/roy/MyApp.kt
+++ app/src/main/java/com/roy/MyApp.kt
@@ -2,17 +2,18 @@ package com.roy
 
 import android.app.Application
 
-//TODO ic_launcher
-//TODO proguard
 //TODO keystore
 //TODO rename app
 //TODO rate, more app, share app
-//TODO leak canary
 //TODO policy
-//TODO ad
+
+//TODO applovin
 
 //done
 //permission manifest
+//ic_launcher
+//proguard
+//leak canary
 
 class MyApp : Application() {
     override fun onCreate() {
